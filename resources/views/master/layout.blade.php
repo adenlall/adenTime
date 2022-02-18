7fa951cb6770d1fa2663f76681975dad263675be
aden lall <91727676+adenlall@users.noreply.github.com>
2022-02-18 03:29:42 +0100

    Add hotjar analytics

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -12,14 +12,23 @@
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
         <meta property="og:image" content="https://ripeemangoes.files.wordpress.com/2017/12/original.jpg" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+        <!-- Hotjar Tracking Code for https://adenlall.herokuapp.com/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2838465,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         @yield('style')
     </head>
     <body>
         @yield('nav')
         @yield('body')
         @yield('footer')
-
 
         @yield('script')
     </body>
